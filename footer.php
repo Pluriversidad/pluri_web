a267a443c1ff99c9d6c6f53fdb0806a313568ccc
Pablo Selín Carrasco Armijo <pabloselin@gmail.com>
2022-10-27 15:54:54 +0200

    footer link to github

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 		<p>Este proyecto cuenta con el apoyo de Fundación Daniel y Nina Carasso</p>
 	<?php endif;?>
 	<div class="enconstruccion">
-		Sitio en construcción v.<?php echo PLURI_VERSION;?>
+		Sitio en construcción <a href="https://github.com/pabloselin/pluri_web">v.<?php echo PLURI_VERSION;?></a>
 	</div>
 </footer>
 <?php wp_footer(); ?>
