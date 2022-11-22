6d59de4176363636337b657df983f303f6ad7b21
Pablo Selin Carrasco Armijo <pabloseline@gmail.com>
2022-11-22 11:53:47 +0100

    custom post types in tag archives

--- functions.php
+++ functions.php
@@ -31,3 +31,13 @@ add_filter( 'get_the_archive_title', function ( $title ) {
 
     return $title;
 });
+
+
+function tag_filter($query) {
+  if ( !is_admin() ) {
+    if ($query->is_tag) {
+      $query->set('post_type', array( 'post', 'calendario', 'cursos', 'cuaderno_de_notas', 'formaciones', 'red_y_consejo' ));
+    }
+  }
+}
+add_action('pre_get_posts','tag_filter');
