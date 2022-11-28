5d72753ffae982b8ea5f1aa6028a06c7c3d64a3a
Pablo Selin Carrasco Armijo <pabloseline@gmail.com>
2022-11-28 16:55:15 +0100

    home tag cloud

--- home.php
+++ home.php
@@ -1,25 +1,18 @@
 <?php get_header(); ?>
 <main id="primary" class="site-main">
 
-    <!-- <div class="home-section">
-        <div class="description">
-            <?php
-            //display home page content
-            $about = get_page(59);
-            echo apply_filters('the_content', $about->post_content);
-            ?>
-        </div>
-
+     <div class="home-section">
+        
         <div class="pluri-tag-cloud">
 
         </div>
 
-    </div> -->
+    </div>
 
 </main>
 
 
-<?php get_template_part('parts/institutos-nav');
+<?php //get_template_part('parts/institutos-nav');
 ?>
 
 <?php get_footer(); ?>
