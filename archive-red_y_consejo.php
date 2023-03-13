3094fb264c3d75787ca3991cf14e181f11a6344f
Pablo Selin Carrasco Armijo <pabloseline@gmail.com>
2023-03-13 08:57:58 +0100

    [red y consejo] ajustes imagenes 3

--- archive-red_y_consejo.php
+++ archive-red_y_consejo.php
@@ -37,9 +37,9 @@
 								<div class="image-container">
 									<?php
 									$thumbnailID = get_post_thumbnail_id($integrante->ID);
-									$thumbSrc = wp_get_attachment_image_url($thumbnailID, 'thumbnail');
+									$thumbSrc = wp_get_attachment_image_url($thumbnailID, 'post-thumbnail');
 									?>
-									<img src="<?= $thumbSrc; ?>" alt="<?= $integrante->post_title; ?>">
+									<img class="integrante-imagen" src="<?= $thumbSrc; ?>" alt="<?= $integrante->post_title; ?>">
 								</div>
 
 								<h3><?php echo get_the_title($integrante->ID); ?></h3>
