c7140321a1790b6a5896d181f8aba0eea0f6b90a
Pablo Selin Carrasco Armijo <pabloseline@gmail.com>
2023-08-10 19:03:40 +0200

    [financiamiento] ajuste financiamiento para lowdef

--- page-template-financiamiento.php
+++ page-template-financiamiento.php
@@ -11,4 +11,13 @@ $content = $args['content'];
 <div class="pluri_financiamiento">
     <h3><?php echo $content->post_title; ?></h3>
     <div><?php echo apply_filters('the_content', $content->post_content); ?></div>
+    <?php if ($_GET['pl_ver'] == 'low') {
+    ?>
+        <ul class="financiamiento-low">
+            <li>Fundación Daniel y Nina Carasso</li>
+            <li>Allianz Foundation</li>
+            <li>Generalitat de Catalunya - Departament de Cultura</li>
+        </ul>
+    <?php
+    }; ?>
 </div>
